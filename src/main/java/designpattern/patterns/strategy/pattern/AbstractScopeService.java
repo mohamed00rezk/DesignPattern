9fccf5080c7e9a8515c4ab9c69aceb1759f03c65
MRezk <Mohammed.Rezk@naseej.com>
2023-09-22 21:26:14 +0300

    init DP Chain od responsibility pattern

--- src/main/java/designpattern/patterns/strategy/pattern/AbstractScopeService.java
+++ src/main/java/designpattern/patterns/strategy/pattern/AbstractScopeService.java
@@ -1,10 +1,6 @@
 package designpattern.patterns.strategy.pattern;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public abstract class AbstractScopeService implements ScopeService {
-  private static final Logger logger = LogManager.getLogger();
 
   public String getScopeQuery() {
     return getScopeKey() + getQueryKey();
